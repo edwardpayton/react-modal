803447e60017aef89ec53e048cd6b328fadbde54
Edward Payton <edwardjpayton@gmail.com>
2019-11-16 19:35:25 +0000

    feat: added prettierignore, commitlint

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  coverageDirectory: '<rootDir>/test/__coverage__/',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|scss|less)$': '<rootDir>/test/__mocks__/styleMock.js',
   },
+  setupFiles: ['<rootDir>/test/__mocks__/shim.js'],
 };
